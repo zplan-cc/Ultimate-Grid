2831807ffe3ccfc3889073e3d4c1228768f2baa9
chenlu <chenl-v@glodon.com>
2018-09-05 19:04:51 +0800

    修改滚动纵向滚动范围小于1000的限制

--- Source/ugvscrol.cpp
+++ Source/ugvscrol.cpp
@@ -3,7 +3,7 @@
 **************************************************************************
 	Source file : UGVScrol.cpp
 // This software along with its related components, documentation and files ("The Libraries")
-// is © 1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
+// is ?1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
 // governed by a software license agreement ("Agreement").  Copies of the Agreement are
 // available at The Code Project (www.codeproject.com), as part of the package you downloaded
 // to obtain this file, or directly from our office.  For a copy of the license governing
@@ -88,65 +88,38 @@ void CUGVScroll::Moved()
 		m_lastScrollMode = m_GI->m_vScrollMode;
 		m_lastNumLockRow = m_GI->m_numLockRows;
 
-		//set up the scrollbar if the number of rows is less than 1000
-		if(m_GI->m_maxTopRow <=1000)
+		if (UG_SCROLLJOYSTICK == m_GI->m_vScrollMode)
 		{
-			if(UG_SCROLLJOYSTICK == m_GI->m_vScrollMode)
-			{
-				SCROLLINFO ScrollInfo;
-				ScrollInfo.cbSize =sizeof(SCROLLINFO);
-				ScrollInfo.fMask = SIF_PAGE | SIF_RANGE |SIF_POS;
-				ScrollInfo.nPage = 0;
-				ScrollInfo.nMin = 0;
-				ScrollInfo.nMax = 2;
-				ScrollInfo.nPos = 1;
-				SetScrollInfo(&ScrollInfo,FALSE);
-				Invalidate();
-			}
-			else
-			{
-				SCROLLINFO ScrollInfo;
-
-				ScrollInfo.cbSize =sizeof(SCROLLINFO);
-				ScrollInfo.fMask = SIF_PAGE | SIF_RANGE;
-
-				if( m_GI->m_defRowHeight < 1 )
-					m_GI->m_defRowHeight =  1;
-
-				ScrollInfo.nPage = m_GI->m_gridHeight / m_GI->m_defRowHeight;
-
-				ScrollInfo.nMin = m_GI->m_numLockRows;
-				ScrollInfo.nMax = m_GI->m_maxTopRow + ScrollInfo.nPage -1;
-				SetScrollInfo(&ScrollInfo,FALSE);
-				Invalidate();
-			}
-			bScrolled = TRUE;
-			m_multiRange = 1;
-			m_multiPos	= 1;			
+			SCROLLINFO ScrollInfo;
+			ScrollInfo.cbSize = sizeof(SCROLLINFO);
+			ScrollInfo.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;
+			ScrollInfo.nPage = 0;
+			ScrollInfo.nMin = 0;
+			ScrollInfo.nMax = 2;
+			ScrollInfo.nPos = 1;
+			SetScrollInfo(&ScrollInfo, FALSE);
+			Invalidate();
 		}
-		//set up the scrollbar if the number of rows is greater than 1000
 		else
 		{
-			m_multiRange = (double)1000 / (double)m_GI->m_maxTopRow;
-			m_multiPos   = (double)m_GI->m_maxTopRow / (double)1000;
-			if(UG_SCROLLJOYSTICK==m_GI->m_vScrollMode)
-			{
-				SetScrollRange(0,2,FALSE);
-				m_multiRange = 1;
-				m_multiPos	= 1;
-			}
-			else
-			{
-				SCROLLINFO ScrollInfo;
-				ScrollInfo.cbSize =sizeof(SCROLLINFO);
-				ScrollInfo.fMask = SIF_PAGE | SIF_RANGE;
-				ScrollInfo.nPage = 1;
-				ScrollInfo.nMin = 0;//m_GI->m_numLockRows;
-				ScrollInfo.nMax = 1000;
-				SetScrollInfo(&ScrollInfo,FALSE);
-			}
-			bScrolled = TRUE;
+			SCROLLINFO ScrollInfo;
+
+			ScrollInfo.cbSize = sizeof(SCROLLINFO);
+			ScrollInfo.fMask = SIF_PAGE | SIF_RANGE;
+
+			if (m_GI->m_defRowHeight < 1)
+				m_GI->m_defRowHeight = 1;
+
+			ScrollInfo.nPage = m_GI->m_gridHeight / m_GI->m_defRowHeight;
+
+			ScrollInfo.nMin = m_GI->m_numLockRows;
+			ScrollInfo.nMax = m_GI->m_maxTopRow + ScrollInfo.nPage - 1;
+			SetScrollInfo(&ScrollInfo, FALSE);
+			Invalidate();
 		}
+		bScrolled = TRUE;
+		m_multiRange = 1;
+		m_multiPos = 1;
 	}
 
 	//set the scroll pos
@@ -273,7 +246,7 @@ Params:
 					  CWnd object being created. 
 Returns:
 	OnCreate must return 0 to continue the creation of the
-	CWnd object. If the application returns –1, the window
+	CWnd object. If the application returns ?, the window
 	will be destroyed.
 *****************************************************/
 int CUGVScroll::OnCreate(LPCREATESTRUCT lpCreateStruct) 
