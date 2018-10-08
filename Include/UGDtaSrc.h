08d353a023a74f99b7e03d3037249364b57cc8e3
chensr <907290005@qq.com>
2018-10-08 11:09:40 +0800

    OnEditFinish中的cancelFlag改成引用

--- Include/UGDtaSrc.h
+++ Include/UGDtaSrc.h
@@ -4,7 +4,7 @@
 	Source file : UGDtaSrc.cpp
 	Header file : UGDtaSrc.h
 // This software along with its related components, documentation and files ("The Libraries")
-// is © 1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
+// is ?1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
 // governed by a software license agreement ("Agreement").  Copies of the Agreement are
 // available at The Code Project (www.codeproject.com), as part of the package you downloaded
 // to obtain this file, or directly from our office.  For a copy of the license governing
@@ -164,7 +164,7 @@ public:
 	// editing notifications...
 	virtual int OnEditStart(int col, long row,CWnd **edit);
 	virtual int OnEditVerify(int col,long row,CWnd *edit,UINT *vcKey);
-	virtual int OnEditFinish(int col, long row,CWnd *edit,LPCTSTR string,BOOL cancelFlag);
+	virtual int OnEditFinish(int col, long row,CWnd *edit,LPCTSTR string,BOOL &cancelFlag);
 	virtual int OnEditContinue(int oldcol,long oldrow,int* newcol,long* newrow);
 
 	//other functions
--- Source/UGDtaSrc.cpp
+++ Source/UGDtaSrc.cpp
@@ -3,7 +3,7 @@
 **************************************************************************
 	Source file : UGDtaSrc.cpp
 // This software along with its related components, documentation and files ("The Libraries")
-// is © 1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
+// is ?1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
 // governed by a software license agreement ("Agreement").  Copies of the Agreement are
 // available at The Code Project (www.codeproject.com), as part of the package you downloaded
 // to obtain this file, or directly from our office.  For a copy of the license governing
@@ -880,7 +880,7 @@ Return:
 	TRUE - to allow the edit it proceede
 	FALSE - to force the user back to editing of that same cell
 ****************************************************/
-int CUGDataSource::OnEditFinish(int col, long row,CWnd *edit,LPCTSTR string,BOOL cancelFlag)
+int CUGDataSource::OnEditFinish(int col, long row,CWnd *edit,LPCTSTR string,BOOL &cancelFlag)
 {
 	UNREFERENCED_PARAMETER(col);
 	UNREFERENCED_PARAMETER(row);
