5253dda869c0bf3521fd9e01e2a2b004becc0618
chenlu <chenl-v@glodon.com>
2019-04-02 10:30:27 +0800

    将CUGVScrol类中的部分成员函数设置为虚函数

--- Include/ugvscrol.h
+++ Include/ugvscrol.h
@@ -4,7 +4,7 @@
 	Source file : ugvscrol.cpp
 	Header file : ugvscrol.h
 // This software along with its related components, documentation and files ("The Libraries")
-// is © 1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
+// is ?1994-2007 The Code Project (1612916 Ontario Limited) and use of The Libraries is
 // governed by a software license agreement ("Agreement").  Copies of the Agreement are
 // available at The Code Project (www.codeproject.com), as part of the package you downloaded
 // to obtain this file, or directly from our office.  For a copy of the license governing
@@ -71,8 +71,8 @@ public:
 
 	//internal functions
 	void Update();
-	void Moved();
-	void VScroll(UINT nSBCode, UINT nPos);
+	virtual void Moved();
+	virtual void VScroll(UINT nSBCode, UINT nPos);
 
 };
 
